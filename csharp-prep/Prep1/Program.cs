04ae713e4ea0dce61e40c45f775c70dfcdd7777c
pnxokratos <her19054@byui.edu>
2023-04-19 22:47:04 +0200

    Finished prep1 exercise.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Enter your first name: ");
+        string firstName = Console.ReadLine();
+        Console.Write("Enter your last name: ");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Your name is {1}, {0}, {1}", firstName, lastName);
     }
 }
